ffd18ca88197b0379349b7b72bbf53d5d19efeb4
Chadgineer <iberker37@gmail.com>
2025-03-19 12:23:56 +0300

    simple changes

--- Assets/Berk/Script/Player.cs
+++ Assets/Berk/Script/Player.cs
@@ -15,17 +15,16 @@ public class Player : MonoBehaviour
     public float moveSpeed = 7.5f;
     public bool key = false;
     public bool PlatformMoving = false;
+    private bool AttackoOnCooldown;
     public int direction; // 1 = left, 2 = right
 
     [SerializeField] private float Health = 100;
     private bool Alive = true;
 
     [SerializeField] private bool grounded = true;
-    [SerializeField] private bool touchingWall = false;
 
     public GameObject GateBlocker;
-    public GameObject LeftMelee;
-    public GameObject RightMelee;
+    public GameObject meleeAttack;  
 
     public Rigidbody2D rigidbodyPlayer;
     private Vector3 originalScale;
@@ -60,20 +59,19 @@ public class Player : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-                newVelocity.x = moveSpeed;
-                transform.localScale = originalScale;
+            newVelocity.x = moveSpeed;
+            transform.localScale = originalScale;
             direction = 2;
         }
         else
         {
-                newVelocity.x = 0;
+            newVelocity.x = 0;
         }
-        if (grounded && !touchingWall && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
+        if (grounded && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
         {
             newVelocity.y = jumpSpeed;
             grounded = false;
         }
-
         rigidbodyPlayer.velocity = newVelocity;
     }
 
@@ -101,8 +99,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButton(0))    
         {
-            if (direction == 1) { StartCoroutine(LeftAttack()); }
-            else if (direction == 2) { StartCoroutine(RightAttack()); }
+            StartCoroutine(Attack());
         }
     }
     IEnumerator TakingDamage() 
@@ -110,20 +107,18 @@ public class Player : MonoBehaviour
         Health -=12.5f;
         yield return new WaitForSeconds(0.5f);   
     }
-    IEnumerator RightAttack() 
-    { 
-        RightMelee.SetActive(true);
-        yield return new WaitForSeconds(0.1f);
-        RightMelee.SetActive(false);
-        yield return new WaitForSeconds(1); 
-    }
 
-    IEnumerator LeftAttack()
+    IEnumerator Attack()
     {
-        LeftMelee.SetActive(true);
-        yield return new WaitForSeconds(0.1f);
-        LeftMelee.SetActive(false);
-        yield return new WaitForSeconds(1);
+        if (AttackoOnCooldown == false) 
+        { 
+            meleeAttack.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+            meleeAttack.SetActive(false);
+            AttackoOnCooldown = true;
+            yield return new WaitForSeconds(0.9f);
+            AttackoOnCooldown = false; 
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -136,13 +131,7 @@ public class Player : MonoBehaviour
             {
                 grounded = true;
             }
-            else if (angle > 45f && angle < 135f)
-            {
-                touchingWall = true;
-            }
-        }
-        
-        
+        } 
 
         if (collision.collider.CompareTag("Key"))
         {
@@ -181,7 +170,6 @@ public class Player : MonoBehaviour
     private void OnCollisionExit2D(Collision2D collision)
     {
         grounded = false;
-        touchingWall = false;
         
         if (collision.collider.CompareTag("PlatformMoving"))
         {
